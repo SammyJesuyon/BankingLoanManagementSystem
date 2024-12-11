d6396e5553db426340cf371aea9884d041f0f7cd
Samson Kitigo <samsonjesuyon@gmail.com>
2024-12-11 01:23:06 -0600

    configuration added for loan management service

---
+++ LoanManagement/src/test/java/org/jesuyon/blms/loanmanagement/LoanManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jesuyon.blms.loanmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
